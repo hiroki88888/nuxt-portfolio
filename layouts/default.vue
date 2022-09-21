de553d88d0348b70c0e004e2150157af50fcb571
hiroki88888 <hiroki1106g@gmail.com>
2022-09-22 02:41:22 +0900

    frist commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      <Header />
+      <main><slot/></main>
+      <Footer />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+//nuxt.config.ts
+
+import { defineNuxtConfig } from'nuxt/config'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+export default defineNuxtConfig
+({
+    modules:["@nuxt/content",
+    ],
+    css: [
+        "~/assets/styles/common.css",
+        "~/assets/styles/layout.css",
+        "~/assets/styles/index.css",
+        "~/assets/styles/blog.css",
+        "~/assets/styles/contact.css",
+        "~/assets/styles/singleblog.css"
+    ]
+
+})
